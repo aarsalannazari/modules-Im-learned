045b9975fee7bcb0da28a6976267be9cc5ca3c46
aarsalannazari <aarsalannazari@gmail.com>
2023-03-13 05:18:04 +0330

    .

---
+++ modules.py
@@ -0,0 +1,60 @@
+# Hack and info and websites :
+
+import dns # get information about dns 
+import ipwhois # get information about websites
+import whois # information about websites
+import requests # this is for request to websites and sometimes get info
+import urllib # we can use it for open and get info from websites
+import urllib3 # we can use it for open and get info from websites
+import builtwith # information about websites
+import socket # its can make socket in system
+import getpass # get password from system
+import getmac # we can use for now MAC Address
+import pynput # we can use it for keylogger
+import pyngrok # we can use it for run an web server
+
+# System libraries :
+
+import os # It can do some system commands
+import sys # It can do some system commands and some inputs from system for example arguments in terminal
+import subprocess # It can do some system commands
+import _thread # we can start another prcess in one program
+import threading # its locks like _thread
+import platform # we can use it for information about platform
+import cryptography # we can encrypt in decrypt some things
+import builtins # .pyi and its a master and its import in python
+
+# Graphics :
+
+import tkinter
+import turtle
+import PyQt5 # with qt is easier
+import PyQt6 # with qt is easier
+import PySide2
+import PySide6
+
+    # this are can make a gui and ui app
+
+# Colors :
+
+import colorama # we can change the color of text in program or terminal
+
+# Edits :
+
+import csv # we can edit .csv files
+
+# Date time :
+
+import time # we can use it for wait few soconds and the other jobs
+import datetime # we can use it for format of datetime or date time of now
+import jdatetime # we can use it for format of datetime or date time of now but its 'shamsi'
+
+# Games :
+
+import pygame # we can use it for make games
+
+# Others :
+
+import random # we can choise a random number with this
+import dropbox # this is library for dropbox
+import pprint # we can use it for making beautiful texts 
